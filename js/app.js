69fcae1a985c132ed5db47e7c890546420afeaf9
booka39 <mohamedaboukaram39@gmail.com>
2023-05-27 02:40:08 +0300

    animate

--- js/app.js
+++ js/app.js
@@ -11,6 +11,14 @@ particlesJS.load('particles-js', 'particles.json', function() {
 
 /* Otherwise just put the config content (json): */
 
+//arrow color 
+
+const svgElement = document.querySelector('.arrow-down');
+
+// Set the fill and stroke color to white
+svgElement.style.fill = '#ffffff';
+svgElement.style.stroke = '#ffffff';
+
 //import elements
 let lis = document.querySelectorAll('.navbar-nav li');
 
@@ -57,126 +65,128 @@ window.addEventListener('scroll', () => {
 
 //background animated
 
-particlesJS('particles-js',
+//particlesJS('particles-js',
 
-    {
-        "particles": {
-            "number": {
-                "value": 80,
-                "density": {
-                    "enable": true,
-                    "value_area": 800
-                }
-            },
-            "color": {
-                "value": "#646364"
-            },
-            "shape": {
-                "type": "circle",
-                "stroke": {
-                    "width": 0,
-                    "color": "#918e8e"
-                },
-                "polygon": {
-                    "nb_sides": 5
-                },
-                "image": {
-                    "src": "img/github.svg",
-                    "width": 100,
-                    "height": 100
-                }
-            },
-            "opacity": {
-                "value": 0.5,
-                "random": false,
-                "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-                }
-            },
-            "size": {
-                "value": 5,
-                "random": true,
-                "anim": {
-                    "enable": false,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": false
-                }
-            },
-            "line_linked": {
-                "enable": true,
-                "distance": 150,
-                "color": "#918e8e",
-                "opacity": 0.4,
-                "width": 1
-            },
-            "move": {
-                "enable": true,
-                "speed": 3,
-                "direction": "none",
-                "random": false,
-                "straight": false,
-                "out_mode": "out",
-                "attract": {
-                    "enable": false,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                }
-            }
-        },
-        "interactivity": {
-            "detect_on": "canvas",
-            "events": {
-                "onhover": {
-                    "enable": true,
-                    "mode": "repulse"
-                },
-                "onclick": {
-                    "enable": true,
-                    "mode": "push"
-                },
-                "resize": true
-            },
-            "modes": {
-                "grab": {
-                    "distance": 400,
-                    "line_linked": {
-                        "opacity": 1
-                    }
-                },
-                "bubble": {
-                    "distance": 400,
-                    "size": 40,
-                    "duration": 2,
-                    "opacity": 8,
-                    "speed": 3
-                },
-                "repulse": {
-                    "distance": 200
-                },
-                "push": {
-                    "particles_nb": 4
-                },
-                "remove": {
-                    "particles_nb": 2
-                }
-            }
-        },
-        "retina_detect": true,
-        "config_demo": {
-            "hide_card": false,
-            "background_color": "#b61924",
-            "background_image": "",
-            "background_position": "50% 50%",
-            "background_repeat": "no-repeat",
-            "background_size": "cover"
-        }
-    }
+//    {
+//        "particles": {
+//            "number": {
+//                "value": 80,
+//                "density": {
+//                    "enable": true,
+//                    "value_area": 800
+//                }
+//            },
+//            "color": {
+//                "value": "#646364"
+//            },
+//            "shape": {
+//                "type": "circle",
+//                "stroke": {
+//                    "width": 0,
+//                    "color": "#918e8e"
+//                },
+//                "polygon": {
+//                    "nb_sides": 5
+//                },
+//                "image": {
+//                    "src": "img/github.svg",
+//                    "width": 100,
+//                    "height": 100
+//                }
+//            },
+//            "opacity": {
+//                "value": 0.5,
+//                "random": false,
+//                "anim": {
+//                    "enable": false,
+//                    "speed": 1,
+//                    "opacity_min": 0.1,
+//                    "sync": false
+//                }
+//            },
+//            "size": {
+//                "value": 5,
+//                "random": true,
+//                "anim": {
+//                    "enable": false,
+//                    "speed": 40,
+//                    "size_min": 0.1,
+//                    "sync": false
+//                }
+//            },
+//            "line_linked": {
+//                "enable": true,
+//                "distance": 150,
+//                "color": "#918e8e",
+//                "opacity": 0.4,
+//                "width": 1
+//            },
+//            "move": {
+//                "enable": true,
+//                "speed": 3,
+//                "direction": "none",
+//                "random": false,
+//                "straight": false,
+//                "out_mode": "out",
+//                "attract": {
+//                    "enable": false,
+//                    "rotateX": 600,
+//                    "rotateY": 1200
+//                }
+//            }
+//        },
+//        "interactivity": {
+//            "detect_on": "canvas",
+//            "events": {
+//                "onhover": {
+//                    "enable": true,
+//                    "mode": "repulse"
+//                },
+//                "onclick": {
+//                    "enable": true,
+//                    "mode": "push"
+//                },
+//                "resize": true
+//            },
+//            "modes": {
+//                "grab": {
+//                    "distance": 400,
+//                    "line_linked": {
+//                        "opacity": 1
+//                    }
+//                },
+//                "bubble": {
+//                    "distance": 400,
+//                    "size": 40,
+//                    "duration": 2,
+//                    "opacity": 8,
+//                    "speed": 3
+//                },
+//                "repulse": {
+//                    "distance": 200
+//                },
+//                "push": {
+//                    "particles_nb": 4
+//                },
+//                "remove": {
+//                    "particles_nb": 2
+//                }
+//            }
+//        },
+//        "retina_detect": true,
+//        "config_demo": {
+//            "hide_card": false,
+//            "background_color": "#b61924",
+//            "background_image": "",
+//            "background_position": "50% 50%",
+//            "background_repeat": "no-repeat",
+//            "background_size": "cover"
+//        }
+//    }
 
-);
+//);
+
+
 //hover on svg icons
 let cards = document.querySelectorAll('#services .card');
 let svgHover = document.querySelectorAll('#services .svg-hover');
