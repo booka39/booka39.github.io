448ebd5f862d22a8d07b854d8b600c254d26329c
booka39 <mohamedaboukaram39@gmail.com>
2023-06-03 03:20:42 +0300

    fade8

--- server.js
+++ server.js
@@ -1,44 +1,40 @@
 const express = require('express');
-const nodemailer = require('nodemailer');
+const path = require('path');
+const { exec } = require('child_process');
 const app = express();
 
 // Middleware to parse JSON data
 app.use(express.json());
 
+// Serve the static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // POST route for form submission
 app.post('/send-email', (req, res) => {
     const { name, email, message } = req.body;
 
-    // Create a transporter for sending emails
-    const transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'mohamedaboukaram39@gmail.com', // Update with your Gmail email address
-            pass: '01093300032Booka' // Update with your Gmail password
-        }
-    });
+    // Create the email content
+    const emailContent = `To: ahmed.shehata.360@gmail.com\nFrom: ${email}\nSubject: New Contact Form Submission\n\nName: ${name}\nEmail: ${email}\nMessage: ${message}`;
 
-    // Create the email
-    const mailOptions = {
-        from: 'mohamedaboukaram39@gmail.com', // Update with your Gmail email address
-        to: 'ahmed.shehata.360@gmail.com',
-        subject: 'New Contact Form Submission',
-        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
-    };
-
-    // Send the email
-    transporter.sendMail(mailOptions, (error, info) => {
+    // Use the sendmail command to send the email
+    exec(`echo -e "${emailContent}" | sendmail -t`, (error) => {
         if (error) {
-            console.log(error);
+            console.error(error);
             res.status(500).json({ success: false, message: 'An error occurred while sending the email.' });
         } else {
-            console.log('Email sent: ' + info.response);
-            res.json({ success: true, message: 'Email sent successfully.' });
+            console.log('Email sent successfully');
+            res.sendFile(path.join(__dirname, 'index.html'));
         }
     });
 });
 
+// Serve the form.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 // Start the server
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
